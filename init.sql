5eafb5345ff6891c191873488b6ea1884c5c4eb3
Adam Kenneweg <akennewe@uwaterloo.ca>
2021-03-20 19:49:03 -0400

    cleanup sql

--- init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Users (
-    Username varchar(255),
-    Password varchar(255),
-    FirstName varchar(255),
-    MiddleName varchar(255),
-    LastName varchar(255),
-    Email varchar(255),
-    Telephone varchar(255)
-);
